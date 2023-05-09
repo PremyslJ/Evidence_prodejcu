73246664b22892b82174b33756116e68f3be71ae
Přemysl Ježek <132379663+PremyslJ@users.noreply.github.com>
2023-05-09 22:15:51 +0200

    oprava výstupu

--- src/com/engeto/invoice/Main.java
+++ src/com/engeto/invoice/Main.java
@@ -7,8 +7,8 @@ public class Main {
 
         String jmenoPrijmeni = "Přemysl Ježek";  // Jmeno, prijmeni dodavatele
         LocalDate datumNarozeni = LocalDate.of(1978, Month.DECEMBER, 21); // datum narozeni
-        int pocetSmluv = 5;                           // pocet uzavrenych smluv
-        double prodanaMrkev = 3.6;                  // celková prodaná mrkev
+        int pocetSmluv = 4;                           // pocet uzavrenych smluv
+        double prodanaMrkev = 3.00;                  // celková prodaná mrkev
         String sidloProdavajici = "Velka Bites";    // bydliste prodavajiciho
         String registZnacka = "6C7 1234";           // registracni znacka
         double spotrebaAuta = 3.8;                  //  spotreba auta na 100 km/l
@@ -16,7 +16,9 @@ public class Main {
 
         double prumProdMrkev = prodanaMrkev/pocetSmluv;
 
-        System.out.println("Průměrné množství prodané mrkve v tunách na jednu smlouvu je  "+ prumProdMrkev+ " Tun" );
+        System.out.println("Průměrné množství prodané mrkve v tunách na jednu smlouvu je  "+prumProdMrkev);
+
+
 
 
 
